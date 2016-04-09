ab1de8711eb9222216cceb0506556934284daad7
Blase Mag <blasemaglaya@gmail.com>
2016-04-09 00:40:43 -0700

    Add cheap usage exit

--- provrdr.py
+++ provrdr.py
@@ -6,19 +6,35 @@ provrdr.py
 import sys
 import csv
 
+def usage_exit(args):
+	numargs = len(args)
+	if numargs < 2 or numargs > 3:
+		sys.exit("usage: provrdr.py infile outfile")
+
+
+usage_exit(sys.argv)
+	
 infile = sys.argv[1]
 outfile = 'outfile.txt'
 
-print(str(infile))
+if len(sys.argv) == 3:
+	outfile = sys.argv[2]
 
 strarr = []
-with open(infile, newline='') as f:
-	reader = csv.reader(f)	
-	for row in reader:
-		strarr.append(row)
-f.close()
+
+try:
+	with open(infile, newline='') as f:
+		reader = csv.reader(f)	
+		for row in reader:
+			strarr.append(row)
+	f.close()
+except IOError:
+	sys.exit('Could not read file: {}\n'.format(infile))
+	
 
 
+print(strarr)
+"""
 list2 = []
 for r in strarr:
 	list2.append(list(filter(None, r)))
@@ -26,8 +42,8 @@ print(list2)
 
 """
 of = open(outfile, 'w')
-of.write(str(list2))
+of.write(str(strarr))
 #for rw in list2:
 #	of.write('{} \n'.format(rw))
 of.close
-"""
+
