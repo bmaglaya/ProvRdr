8cb5b25b26b82ba4db72ef037a4345ef58da7a18
Blase Mag <blasemaglaya@gmail.com>
2016-04-08 17:33:07 -0700

    Remove extraneous items from list

--- provrdr.py
+++ provrdr.py
@@ -7,12 +7,27 @@ import sys
 import csv
 
 infile = sys.argv[1]
+outfile = 'outfile.txt'
+
 print(str(infile))
 
+strarr = []
 with open(infile, newline='') as f:
-	reader = csv.reader(f)
-	of = open('outfile.txt', 'w')
+	reader = csv.reader(f)	
 	for row in reader:
-		of.write('{} \n'.format(str(row)))
-	of.close()
+		strarr.append(row)
 f.close()
+
+
+list2 = []
+for r in strarr:
+	list2.append(list(filter(None, r)))
+print(list2)
+
+"""
+of = open(outfile, 'w')
+of.write(str(list2))
+#for rw in list2:
+#	of.write('{} \n'.format(rw))
+of.close
+"""
