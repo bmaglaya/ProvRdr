2e6f5c31eab4e5c93ff43392a81d0b410a530ed1
Blase Mag <blasemaglaya@gmail.com>
2016-04-10 15:41:02 -0700

    Styling

--- provrdr.py
+++ provrdr.py
@@ -10,14 +10,14 @@ import csv
 
 parser = argparse.ArgumentParser(description="Process ProvidentCU bank statements.")
 parser.add_argument("infile", 
-					type=str, 
-					default=sys.stdin,
-					help=".cvs file to be read in")
+	type=str, 
+	default=sys.stdin,
+	help=".cvs file to be read in")
 parser.add_argument("outfile", 
-					nargs='?', 
-					type=str, 
-					default='outfile.txt',
-					help="optional output file")
+	nargs='?', 
+	type=str, 
+	default='outfile.txt',
+	help="optional output file")
 args = parser.parse_args()
 	
 infile = args.infile
